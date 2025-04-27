678b4cbd958b8b62a19872d24314ca27ce490e53
延岡あやか <nobeokaayaka@nobeokaayakanoMacBook-Air.local>
2025-04-27 16:12:26 +0900

    コミット

--- day5/vite-project/src/App.tsx
+++ day5/vite-project/src/App.tsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
-  return (
-    <>
+const [input,setInput] = useState("");
+return (
+    <div>
     <h1>Todo App Practice</h1>
+    <input value={input}></input>
+    <button>追加</button>
 
-    </>
+      <ul>
+        <li>reactの勉強<button>削除</button></li>
+      </ul>
+    </div>
   )
 }
 
--- day5/vite-project/src/main.tsx
+++ day5/vite-project/src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+const root = document.getElementById('root');
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+if (root) {
+  createRoot(root).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
